613d6460fb4da71ec45f0fe303fdd2ab802683c6
Denny Pearce <digital.y.coffee@gmail.com>
2022-04-29 21:13:34 +0300

    add images and videos to project, start writing header nav component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Nav>
+      <Logo src='/images/logo.svg' />
+      <NavMenu>
+        <a href='#home'>
+          <img src='/images/home-icon.svg' alt='home icon' />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 };
 
 export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+  }
+`;
