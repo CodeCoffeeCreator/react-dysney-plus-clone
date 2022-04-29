d0f4b91ee10cdd2aee4748ab6b13ee922a4b93b0
Denny Pearce <digital.y.coffee@gmail.com>
2022-04-30 02:14:07 +0300

    write simple companies component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Companies from './Companies';
 import ImgSlider from './ImgSlider';
 
 const Home = () => {
   return (
     <Container>
       <ImgSlider />
+      <Companies />
     </Container>
   );
 };
